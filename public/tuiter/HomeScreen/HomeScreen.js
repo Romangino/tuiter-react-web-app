dcf26c8e79d7a3f7f4c68306d45091007287a304
Gino <romangino@gmail.com>
2022-10-14 10:22:14 -0400

    Finished: - Removed NavigationSidebarItems.js - Removed item.js - Fixed NavigationSidebar.js to load in complete navigation bar

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -8,7 +8,7 @@ function HomeScreen() {
     <div class="row mt-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
    <vl></vl>
-    ${NavigationSidebar('Home')}
+    ${NavigationSidebar('home')}
    </div>
    <div class="col-10 col-lg-7 col-xl-6">
    ${PostList()}
